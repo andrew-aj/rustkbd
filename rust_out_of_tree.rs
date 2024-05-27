503d24476937bfc56ad2dc46605851f84c0da90b
andrew-aj <andrew.knee.j@gmail.com>
2024-05-27 19:51:48 -0400

    almost to driver initialization

--- rust_out_of_tree.rs
+++ rust_out_of_tree.rs
@@ -3,18 +3,73 @@
 //! Rust out-of-tree sample
 
 use core::cell::UnsafeCell;
+use core::marker::PhantomData;
 
 use kernel::bindings;
-use kernel::error::VTABLE_DEFAULT_ERROR;
 use kernel::prelude::*;
+use kernel::error::*;
 use kernel::types::Opaque;
+
+macro_rules! module_device_table {
+    ($type:ty, $name:ident) => {
+        #[no_mangle]
+        pub static __mod_{$type}__{$name}_device_table: *const $type = &$name as *const _;
+    };
+}
+
+pub const fn create_usb_driver<T: UsbDriver>() -> UsbDriver {
+    UsbDriver(Opaque::new(bindings::usb_driver {
+
+    }))
+}
+
+struct Module {
+    _reg: UsbRegistration,
+}
+
+pub struct UsbRegistration{
+    driver: Pin<&'static mut UsbDriver>
+}
+
+impl UsbRegistration {
+    pub fn register(module: &'static kernel::ThisModule, driver: Pin<&'static mut UsbDriver>) -> Result<Self> {
+        to_result(unsafe {
+            bindings::usb_register_driver(driver.0.get(), &mut kernel::bindings::__this_module as *mut _, b"rust".as_ptr() as *const i8)
+        })?;
+
+        Ok(UsbRegistration { driver })
+    }
+}
+
+impl Drop for UsbRegistration {
+    fn drop(&mut self){
+        unsafe {
+            bindings::usb_deregister(self.driver.0.get())
+        };
+    }
+}
+
+struct UsbAdapter<T: UsbDriverTrait> {
+    _p: PhantomData<T>,
+}
+
+impl<T: UsbDriverTrait> UsbAdapter<T> {
+    unsafe extern "C" fn probe_callback(intf: *mut bindings::usb_interface, id: *mut bindings::usb_device_id) -> core::ffi::c_int {
+        kernel::error::from_result(|| {
+            let _intf = unsafe {UsbInterface::from_raw(intf)};
+            let _id = unsafe {UsbDeviceId::from_raw(id)};
+            T::probe(_intf, _id)?;
+            Ok(0)
+        })
+    }
+}
 
 const __LOG_PREFIX: &[u8] = b"rust_out_of_tree\0";
 #[cfg(MODULE)]
 #[used]
 static __IS_RUST_MODULE: () = ();
 
-static mut __MOD: Option<RustOutOfTree> = None;
+static mut __MOD: Option<Module> = None;
 
 #[cfg(MODULE)]
 static THIS_MODULE: kernel::ThisModule = unsafe {
@@ -35,7 +90,7 @@ pub extern "C" fn cleanup_module() {
 }
 
 fn __init() -> core::ffi::c_int {
-    match <RustOutOfTree as kernel::Module>::init(&THIS_MODULE) {
+    match <Module as kernel::Module>::init(&THIS_MODULE) {
         Ok(m) => {
             unsafe {
                 __MOD = Some(m);
@@ -59,16 +114,43 @@ fn __exit() {
 #[used]
 pub static __rust_out_of_tree_0: [u8; 12] = *b"license=GPL\0";
 
-// pub struct UsbDriver(UnsafeCell<bindings::usb_driver>);
+pub struct UsbDriver(Opaque<bindings::usb_driver>);
 
 pub struct UsbInterface(Opaque<bindings::usb_interface>);
 
+impl UsbInterface {
+    unsafe fn from_raw<'a>(ptr: *mut bindings::usb_interface) -> &'a mut Self {
+        let ptr = ptr.cast::<Self>();
+
+        unsafe {&mut *ptr}
+    }
+}
+
 pub struct UsbDeviceId(Opaque<bindings::usb_device_id>);
+
+impl UsbDeviceId {
+    unsafe fn from_raw<'a>(ptr: *mut bindings::usb_device_id) -> &'a mut Self {
+        let ptr = ptr.cast::<Self>();
+
+        unsafe {&mut *ptr}
+    }
+}
 
 pub enum PmMessage {}
 
+struct Keyboard;
+
+impl UsbDriverTrait for Keyboard {
+    const Name: &'static CStr = c_str!("UsbDriverTest");
+    const ID_TABLE: Box[UsbDeviceId] =
+}
+
 #[vtable]
-pub trait UsbDriver {
+pub trait UsbDriverTrait {
+    const NAME: &'static CStr;
+
+    const ID_TABLE: Box<[UsbDeviceId]>;
+
     fn probe(intf: &mut UsbInterface, id: &mut UsbDeviceId) -> Result {
         kernel::build_error!(VTABLE_DEFAULT_ERROR)
     }
@@ -102,20 +184,16 @@ pub trait UsbDriver {
 }
 
 
-struct RustOutOfTree {
-    numbers: Vec<i32>,
-}
+impl kernel::Module for Module {
+    static mut DRIVER: UsbDriver = create_usb_driver<Keyboard>();
 
-impl kernel::Module for RustOutOfTree {
     fn init(_module: &'static ThisModule) -> Result<Self> {
-        pr_info!("Rust out-of-tree sample (init)\n");
+        pr_info!("Rust usb driver init");
 
-        let mut numbers = Vec::new();
-        numbers.push(72, GFP_KERNEL)?;
-        numbers.push(108, GFP_KERNEL)?;
-        numbers.push(200, GFP_KERNEL)?;
+        let driver = unsafe {&mut DRIVER};
+        let mut reg = UsbRegistration::register(_module, Pin::static_mut(driver))?;
 
-        Ok(RustOutOfTree { numbers })
+        Ok(Modue{ _reg: reg})
     }
 }
 
